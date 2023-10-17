60e9db1ba808d610beb7afad74cf8b6229ab732c
Haoqi Wang <haoqiwang970205@gmail.com>
2023-10-16 19:01:44 -0500

    add active learning classes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # uvicorn main:app --reload 
+import uvicorn
+
 import sqlite3, config
 
 from fastapi import FastAPI, Request, Form, Query
@@ -36,12 +38,13 @@ def read_pd(table_name: str) -> pd.DataFrame:
     cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM {table_name}")
     table_db = cursor.fetchall()
-    
+
     column_names = [description[0] for description in cursor.description]
     table_pd = pd.DataFrame(table_db, columns=column_names)
     return table_pd
 
-def random_select() -> tuple[str, str]:
+from typing import Tuple
+def random_select() -> Tuple[str, str]:
     df = read_pd('imgdb')
 
     # random select 2 images
@@ -206,6 +209,194 @@ async def submit_experiment(selected_visit: int = Form(...), selected_parameter:
     return RedirectResponse(url="/experimentslist", status_code=303)
 
 
+import numpy as np
+from typing import Tuple, Optional
+import ast
+import json
+
+class Database():
+    def __init__(self, experiment_id: int = None):
+        self.experiment_id = experiment_id
+
+        # perceptual map related parameters
+        self.embedding = read_pd('perceptual_map')[['x', 'y']].to_numpy()  # select x, y columns
+        # self.embedding = np.genfromtxt('best_embedding_2023-08-31.csv', delimiter=',')
+
+        self.N = self.embedding.shape[0]  # # N, number of items, used by ActiveQuery
+        self.D = self.embedding.shape[1]  # number of dimensions of perceptual map
+        self.bounds = [-0.5, 0.5]  # todo: get this according to perceptual map? may need to update stan model
+
+        # bayes model related parameters, read from database, shared by all
+        # self.k = 5.329
+        # self.k_normalization = 'DECAYING'
+        # self.noise_model = 'BT'
+        experiments_df = read_pd('experiments')
+        parameter_id = experiments_df.query('id == @experiment_id')['parameter_id'].item()
+        
+        parameters_df = read_pd('parameters')
+        parameters = parameters_df.query('id == @parameter_id')
+        self.k = parameters['k'].item()
+        self.k_normalization = parameters['response_model'].item()
+        self.noise_model = parameters['probability_model'].item()
+            
+        # self.k = 5.329
+        self.Nsamples = 4000  # number of samples
+
+        # active query related parameters
+        self.Npairs = 50  # random select how many pairs
+
+        # number of rounds done
+        number_rounds = get_number_rounds(experiment_id)
+        if number_rounds == 0:
+            self._start()  # activequery
+        else:
+            # read database
+            self._continue()  # activequery
+
+    def _start(self):
+        # if start new, initialization
+        self.A = []  # list
+        self.tau = []  # numpy list
+        self.y_vec = []  # list
+        self.response = []
+
+        self.W_samples = np.random.uniform(self.bounds[0], self.bounds[1], (self.Nsamples, self.D))
+        
+        self.mu_W = np.mean(self.W_samples, 0)
+        self.Wcov = np.cov(self.W_samples, rowvar=False)  # todo: just give a number?
+        self.A_sel = 0  # self.A[-1]
+        self.tau_sel = 0  # self.tau[-1]
+
+    def _continue(self):
+        # if continue, read database
+        trials = read_pd('newtrials').query('experiment_id == @self.experiment_id') # todo: change to trials
+        
+        self.A = [np.array(ast.literal_eval(s)) for s in trials['a'].tolist()]  # list
+        self.tau = [np.float64(s) for s in trials['tau'].tolist()]  # numpy list
+        self.y_vec = list(np.where(trials['select_id'] == trials['img1_id'], 1, 0))  # list
+        self.response = trials['select_id'].tolist()
+
+        self.mu_W = [np.array(ast.literal_eval(s)) for s in trials['mean'].tolist()][-1]
+        self.Wcov = [np.array(ast.literal_eval(s)) for s in trials['cov'].tolist()][-1]  # only need the last one
+        self.A_sel = self.A[-1]
+        self.tau_sel = self.tau[-1]
+    
+    def initial_estimate(self) -> dict:
+        estimation = {'mean': self.mu_W, 'a': self.A_sel, 'tau': self.tau_sel, 'cov': self.Wcov}
+        return estimation
+    
+    def update(self, estimation: dict, query: Tuple[int, int], response: int):
+        self.mu_W = estimation['mean']
+        self.Wcov = estimation['cov']
+
+        self.A_sel = estimation['a']
+        self.tau_sel = estimation['tau']
+        self.A.append(self.A_sel)
+        self.tau.append(self.tau_sel)
+
+        self.query = query # todo:
+        self.response.append(response)
+        response_binary = 1 if response == query[0] else 0
+        self.y_vec.append(response_binary)
+
+        # write to database
+        # query, response
+        # 'mean': self.mu_W, 'a': self.A_sel, 'tau': self.tau_sel, 'cov': self.Wcov
+
+        # connection = sqlite3.connect(config.DB_FILE)
+        # cursor = connection.cursor()
+        # cursor.execute(
+        # """
+        # INSERT INTO newtrials (experiment_id, round, img1_id, img2_id, select_id, timepoint, mean, cov, a, tau) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        # """, (1, 1, 2, 3, 2, '2023-04-10 10:39:37', json.dumps(mean.tolist()), json.dumps(cov.tolist()), json.dumps(a.tolist()), tau)
+        # )
+
+def pair2hyperplane(p, embedding: np.ndarray, normalization: str, slice_point=None):
+    # converts pair to hyperplane weights and bias
+    A_emb = 2*(embedding[p[0], :] - embedding[p[1], :])  # a, 2(p-q)
+
+    if slice_point is None:
+        tau_emb = (np.linalg.norm(embedding[p[0], :])**2 - np.linalg.norm(embedding[p[1], :])**2)  # b, ||p||^2 - ||q||^2
+    else:
+        tau_emb = np.dot(A_emb, slice_point)
+
+    if normalization == 'CONSTANT':  # normalization
+        pass
+    elif normalization == 'NORMALIZED':
+        A_mag = np.linalg.norm(A_emb)  # ||a||
+        A_emb = A_emb / A_mag
+        tau_emb = tau_emb / A_mag
+    elif normalization == 'DECAYING':
+        A_mag = np.linalg.norm(A_emb)
+        A_emb = A_emb * np.exp(-A_mag)
+        tau_emb = tau_emb * np.exp(-A_mag)
+    return (A_emb, tau_emb)
+
+
+class ActiveQuery():
+    def __init__(self, db: Database, method: str):
+        # input, fixed, k, method, normalization, Npairs, lambda_pen_EPMV
+        self.db = db
+
+        self.method = method
+        self.lambda_pen_MCMV = 1
+
+    def get_next_round(self, Wcov: Optional[np.ndarray] = None):
+        # just need the previous one step, no need to know history
+        lambda_pen_MCMV = self.lambda_pen_MCMV
+        embedding = self.db.embedding
+        k_normalization = self.db.k_normalization
+        k = self.db.k
+        mu_W = self.db.mu_W
+
+        N = self.db.N
+        Npairs = self.db.Npairs
+
+        # generate all possible pairs
+        # Pairs = self.get_all_pairs(self.N)
+
+        # generate random proposal pairs
+        Pairs = self.get_random_pairs(N, Npairs)
+
+        # evaluate proposed pairs
+        if self.method == 'RANDOM':
+            p = Pairs[0]
+        elif self.method == 'MCMV':
+            value = np.zeros((Npairs,))
+
+            for j in range(Npairs):
+                p = Pairs[j]  # pair j
+                (A_emb, tau_emb) = pair2hyperplane(p, embedding, k_normalization)  # get normalized a and b as in paper
+                varest = np.dot(A_emb, Wcov).dot(A_emb)  # first part of eta
+                distmu = np.abs((np.dot(A_emb, mu_W) - tau_emb)  / np.linalg.norm(A_emb))  # second part of eta
+
+                # choose highest variance, but smallest distance to mean
+                value[j] = k * np.sqrt(varest) - lambda_pen_MCMV * distmu
+
+            p = Pairs[np.argmax(value)]
+        
+        query = p
+
+        print(f"Show pair {query}")
+        return query
+    
+    def get_random_pairs(self, N, M):
+        # pair selection support function
+        # N: sample how many pairs
+        # M: Npairs
+        indices = np.random.choice(N, (int(1.5*M), 2))
+        indices = [(i[0], i[1]) for i in indices if i[0] != i[1]]
+        assert len(indices) >= M
+        return indices[0:M]
+
+    def get_all_pairs(self, N):
+        # pair selection support function
+        # N: sample how many pairs
+        import itertools
+        indices = list(itertools.combinations(range(N), 2))
+        return indices
+    
+
 @app.get('/trial')
 # @app.get('/trial?selected_experiment={selected_experiment}&round_count={round_count}')
 # def trial(request: Request):
@@ -222,6 +413,22 @@ def trial(request: Request, selected_experiment: int = Query(...)):
     # random select
     img1, img2 = random_select()
     pred = "/temporary/search.png"
+
+    # active select
+    # input: need experiment_id, read the database, find what images should be shown/calculate here based on previous database info, but this repeats many variables, I maybe should do all of this in submit-trial.html
+    # output: path to 2 images, plot
+
+    # get parameter by selected_experiment
+    db = Database(experiment_id=selected_experiment)
+    # initialize ActiveQuery based on given parameters
+    aq = ActiveQuery(db, 'MCMV')
+    # get next round
+    estimation = db.initial_estimate()  # maybe no need to do this? no, initialization still need?
+    query = aq.get_next_round(estimation['cov'])
+    imgdb_pd = read_pd('imgdb')
+    img1 = "/img_database_2d/" + imgdb_pd.query('img_id == @query[0]')['img_name'].item()  # todo: item
+    img2 = "/img_database_2d/" + imgdb_pd.query('img_id == @query[1]')['img_name'].item()  # todo: item
+
     return templates.TemplateResponse("trial.html", {"request": request, "img1": img1, "img2": img2, "pred": pred})
 
 def get_number_rounds(experiment_id: int):
@@ -230,7 +437,156 @@ def get_number_rounds(experiment_id: int):
 
     cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM trials WHERE experiment_id = {experiment_id}")
-    return len(cursor.fetchall()) + 1
+    return len(cursor.fetchall())
+
+# trial('request', 1)
+
+import matplotlib.pyplot as plt
+import pickle
+import pystan
+
+# import multiprocessing
+# multiprocessing.set_start_method("fork")
+
+class BayesEstimate():
+    stan_model = """
+    data {
+        int<lower=0> D;       // space dimension
+        int<lower=0> M;       // number of measurements so far
+        real k;               // logistic noise parameter (scale)
+        vector[2] bounds;      // hypercube bounds [lower,upper]
+        int y[M];             // measurement outcomes
+        vector[D] A[M];       // hyperplane directions
+        vector[M] tau;        // hyperplane offsets
+    }
+    parameters {
+        vector<lower=bounds[1],upper=bounds[2]>[D] W;         // the user point
+    }
+    transformed parameters {
+        vector[M] z;
+        for (i in 1:M)
+            z[i] = dot_product(A[i], W) - tau[i];
+    }
+    model {
+        // prior
+        W ~ uniform(bounds[1],bounds[2]);
+    
+        // linking observations
+        y ~ bernoulli_logit(k * z);
+    }
+    """
+
+    def __init__(self, db: Database):
+        # load Stan model
+        self.sm = pickle.load(open('model.pkl', 'rb'))
+        print("Loaded saved model")
+
+        """
+        # to make model
+        self.sm = pystan.StanModel(model_code=self.my_model)
+        with open('model.pkl', 'wb') as f:
+            pickle.dump(self.sm, f)
+        """
+
+        self.db = db  # input, read from database, A, tau, y_vec
+
+        # parameters used for bayes sampling
+        self.Nchains = 4  # number of chains to sample
+        self.Niter = int(2*self.db.Nsamples/self.Nchains)  # number of iterations   
+
+    def fit(self, query: Tuple[int, int], response: int) -> dict:
+        embedding = self.db.embedding
+        k_normalization = self.db.k_normalization
+
+        (A_sel, tau_sel) = pair2hyperplane(query, embedding, k_normalization)
+
+        A = self.db.A + [A_sel]  # self.db.A.append(A_sel)
+        tau = np.append(self.db.tau, tau_sel)
+
+        response_binary = 1 if response == query[0] else 0
+        y = response_binary
+        y_vec = self.db.y_vec + [y]
+
+        D = self.db.D
+        k = self.db.k
+        bounds = self.db.bounds
+        
+        # given measurements 0..i, get posterior samples
+        data_gen = {'D': D,  # number of dimensions
+                    'k': k,  # noise constant
+                    'M': len(A),  # number of measurements so far
+                    'A': A, 
+                    'tau': tau,
+                    'y': y_vec,
+                    'bounds': bounds}
+
+        # get posterior samples
+        print('Start fitting...')
+
+        fit = self.sm.sampling(data=data_gen, iter=self.Niter, chains=self.Nchains, init=0)
+        W_samples = fit.extract()['W']
+
+        self.W_samples = W_samples
+        self.mu_W = np.mean(W_samples, 0)
+        self.Wcov = np.cov(self.W_samples, rowvar=False)  # get covariance
+
+        self.A_sel = A_sel
+        self.tau_sel = tau_sel
+
+        estimation = {'mean': self.mu_W, 'a': self.A_sel, 'tau': self.tau_sel, 'cov': self.Wcov}
+        return estimation
+
+    def plot(self, query: Tuple[int, int], response: int, user=None):
+        W_samples = self.W_samples
+        A_sel = self.A_sel#self.A[-1]
+        tau_sel = self.tau_sel#self.tau[-1]
+        embedding = self.db.embedding
+        
+        plt.figure(189)
+        plt.clf()
+        plt.axis([-0.6, 0.6, -0.6, 0.6])
+        plt.gca().set_aspect('equal')
+
+        # split left or right side
+        Nsplit = 0
+        Isplit = []
+        for j in range(1, W_samples.shape[0]):
+            z = np.dot(A_sel, W_samples[j, :]) - tau_sel
+            if z > 0:
+                Isplit.append(j)
+                Nsplit += 1
+
+        # plot estimate
+        estimate_point = self.mu_W
+        plt.scatter(estimate_point[0], estimate_point[1], s=50, c='r', marker='x', zorder=2)
+        # plot posterior
+        plt.plot(W_samples[:, 0], W_samples[:, 1], 'y.', alpha=0.05, zorder=1)
+        # plot posterior other half different color
+        plt.plot(W_samples[Isplit, 0], W_samples[Isplit, 1], 'c.', alpha=0.05, zorder=1)
+        # plot groundtruth
+        if user is not None and user.ideal_point is not None:
+            plt.plot(user.ideal_point[0], user.ideal_point[1], 'b*')
+        # plot landmarks
+        plt.scatter(embedding[:, 0], embedding[:, 1], s=25, c='black', zorder=2)
+        # plot pair shown this round
+        plt.scatter(embedding[query[0], 0], embedding[query[0], 1], s=25, c='m', zorder=2)
+        plt.scatter(embedding[query[1], 0], embedding[query[1], 1], s=25, c='m', zorder=2)
+        # plot response
+        plt.scatter(embedding[response, 0], embedding[response, 1], s=80, facecolors='none', edgecolors='r', zorder=2)
+
+        print(f"Current estimate: {estimate_point}")
+
+        plt.ion()
+
+        # save plots
+        import os
+        folder_name = "random_search2023-10-03"
+        plot_name = f"{folder_name}/{len([name for name in os.listdir(f'./{folder_name}')]):02d}.png"
+        plt.savefig(plot_name, dpi=300)
+
+        # plt.pause(self.plot_pause)  # for observation
+        plt.pause(0.5)  # for observation
+
 
 @app.post("/submit-trial")
 async def submit_trial(selected_image: str = Form(...), img1: str = Form(...), img2: str = Form(...), selected_experiment: int = Form(...)):
@@ -254,19 +610,46 @@ async def submit_trial(selected_image: str = Form(...), img1: str = Form(...), i
         print("Button clicked: img2right")
         select_id = img2_id
 
+    # estimate user point
+    db = Database(experiment_id)
+    be = BayesEstimate(db)
+    # input: previous selections
+    # output: figure, mean, std
+    # do not show the same pair appeared before
     # write to database
+    query = (img1_id, img2_id)
+    response = selected_image
+    estimation = be.fit(query, response)
+
+    # db.update(estimation, query, response)
+    # todo: install python package
+
+    # todo: experiment_id, round, img1_id, img2_id are known in trial.html, the record should be updated, fill in meanx, meany, stdx, stdy
     connection = sqlite3.connect(config.DB_FILE)
     cursor = connection.cursor()
     
+    # cursor.execute(
+    # """
+    # INSERT INTO trials (experiment_id, round, img1_id, img2_id, select_id, timepoint, meanx, meany, stdx, stdy) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    # """, (experiment_id, round, img1_id, img2_id, select_id, 
+    #       datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
+    #       0.20, -0.23, 0.19, 0.21)
+    # )
+
+    #####
+    # mean = np.array([0.08756645, 0.00019115])
+    # cov = np.array([[0.08257532, -0.00073675], [-0.00073675, 0.08365184]])
+    # a = np.array([-0.3654387, 0.01344264])
+    # tau = 0.002649889696594203
+
+    connection = sqlite3.connect(config.DB_FILE)
+    cursor = connection.cursor()
     cursor.execute(
     """
-    INSERT INTO trials (experiment_id, round, img1_id, img2_id, select_id, timepoint, meanx, meany, stdx, stdy) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-    """, (experiment_id, round, img1_id, img2_id, select_id, 
-          datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
-          0.20, -0.23, 0.19, 0.21)
+    INSERT INTO newtrials (experiment_id, round, img1_id, img2_id, select_id, timepoint, mean, cov, a, tau) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """, (experiment_id, round+1, img1_id, img2_id, select_id, '2023-04-10 10:39:37', json.dumps(estimation['mean'].tolist()), json.dumps(estimation['cov'].tolist()), json.dumps(estimation['a'].tolist()), estimation['tau'])
     )
-
-    # todo: update experiments round count
+    #####
     connection.commit()
 
     return RedirectResponse(url=f"/trial?selected_experiment={selected_experiment}", status_code=303)
@@ -311,3 +694,6 @@ async def add_patient(number: int = Form(...), race: str = Form(...), ethnicity:
 
     return RedirectResponse(url="/patients", status_code=303)
 
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
